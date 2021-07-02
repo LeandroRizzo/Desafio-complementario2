11a66800dae275532c1ff9aefa1af59dd533b46e
Leandro Rizzo <86168414+LeandroRizzo@users.noreply.github.com>
2021-07-02 02:01:01 -0300

    Update miarchivo.js

--- miarchivo.js
+++ miarchivo.js
@@ -1,36 +1,3 @@
-/*
-class Producto {
-    constructor(nombre,precio){
-        this.nombre = nombre;
-        this.precio = precio
-    }
-}
-
-const Producto1 = new Producto("Remera", 5000);
-const Producto2 = new Producto("Mate", 1000);
-const Producto3 = new Producto("Pelota", 1700);
-const Producto4 = new Producto("Auto", 5000000);
-
-const Productos = []
-
-Productos.push(Producto1)
-Productos.push(Producto2)
-Productos.push(Producto3)
-Productos.push(Producto4)
-
-let ProductosOrdenados = Productos.sort(function(a, b){
-    if (a.nombre > b.nombre){
-        return 1;
-    }
-    if (a.nombre < b.nombre){
-        return -1;
-    }
-    return 0;
-});
-
-console.log(ProductosOrdenados);
-*/
-
 class Producto{
     constructor(tipo,marca,tamaño,precio){
         this.tipo = tipo,
